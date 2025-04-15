b01b4676e00aa7c8a1860f20360d3018f1fe6ef6
Arthur <arthur_r_rodrigues@hotmaail.com>
2025-04-15 19:54:21 -0300

    processo-seletivo-completo

---
+++ Contador de números pares 0 a 100/src/ContadorPares.java
@@ -0,0 +1,16 @@
+public class ContadorPares {
+    public static void main(String[] args) throws Exception {
+     
+        int numero = 0;
+
+        for(int i = 0; i < 100; i++){
+        if (i % 2 != 0){}
+
+        else{
+            System.out.println(numero);
+        }
+        numero++;
+        
+    }
+    }
+}

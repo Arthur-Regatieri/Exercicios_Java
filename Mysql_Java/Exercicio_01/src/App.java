29bdca43e3905fd67e7075367cdab06d36f8cdf3
Arthur <arthur_r_rodrigues@hotmaail.com>
2025-09-17 21:09:16 -0300

    Finalizado

---
+++ Mysql_Java/Exercicio_01/src/App.java
@@ -0,0 +1,8 @@
+import java.sql.Connection;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+    }
+}
